c4adcf48f4e572e02774c73fb602017e83f223b1
A-arnaavv <saketarnav2005@gmail.com>
2023-08-15 10:29:14 +0530

    Updated Constants.swift

--- Spotify/Utils/Constants.swift
+++ Spotify/Utils/Constants.swift
@@ -16,7 +16,7 @@ class Constants {
     static let tokenAPIURL = "https://accounts.spotify.com/api/token"
 
     static let scopes = "user-read-private%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-follow-read%20user-library-read%20user-library-modify%20user-read-email%20user-top-read%20user-read-recently-played"
-    static let redirectURI = "https://www.iosacademy.io"
+    static let redirectURI = "https://www.apple.com/in/apple-music/"
 
     static let profileUrl = baseAPIURL + "/me"
     static let userTracksUrl = baseAPIURL + "/me/tracks"
